8d325db06ec1302f3dd246cd20ed93d8f4bc82c1
Watthachai Taechalue <wattchaichai@gmail.com>
2023-04-30 01:03:52 +0700

    <feat>: Add UI - using React

--- backend/models.py
+++ backend/models.py
@@ -6,17 +6,19 @@ import passlib.hash as _hash
 
 import database as _database
 
+
 class User(_database.Base):
     __tablename__ = "users"
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
     email = _sql.Column(_sql.String, unique=True, index=True)
     hashed_password = _sql.Column(_sql.String)
-    
+
     leads = _orm.relationship("Lead", back_populates="owner")
-    
+
     def verify_password(self, password: str):
         return _hash.bcrypt.verify(password, self.hashed_password)
-    
+
+
 class Lead(_database.Base):
     __tablename__ = "leads"
     id = _sql.Column(_sql.Integer, primary_key=True, index=True)
@@ -24,8 +26,9 @@ class Lead(_database.Base):
     first_name = _sql.Column(_sql.String, index=True)
     last_name = _sql.Column(_sql.String, index=True)
     email = _sql.Column(_sql.String, index=True)
-    note = _sql.Column(_sql.String, index=True, default="")
+    company = _sql.Column(_sql.String, index=True, default="")
+    note = _sql.Column(_sql.String, default="")
     date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
     date_last_updated = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
-    
+
     owner = _orm.relationship("User", back_populates="leads")
