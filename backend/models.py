c2a83bb1f1e255a5bd2c995ffb4b46c234ba718f
Watthachai Taechalue <wattchaichai@gmail.com>
2023-05-02 17:46:27 +0700

    <feat>: Rework database <add> things like netflix

--- backend/models.py
+++ backend/models.py
@@ -14,6 +14,7 @@ class User(_database.Base):
     hashed_password = _sql.Column(_sql.String)
 
     leads = _orm.relationship("Lead", back_populates="owner")
+    subscriptions = _orm.relationship("Subscription", back_populates="user")
 
     def verify_password(self, password: str):
         return _hash.bcrypt.verify(password, self.hashed_password)
@@ -32,3 +33,76 @@ class Lead(_database.Base):
     date_last_updated = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
 
     owner = _orm.relationship("User", back_populates="leads")
+
+class Content(_database.Base):
+    __tablename__ = "contents"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    catagory_id = _sql.Column(_sql.String, index=True)
+    
+    series = _orm.relationship("Series", back_populates="Catagory")
+
+class Series(_database.Base):
+    __tablename__ = "series"
+    id = _sql.Column(_sql.Integer, unique=True, index=True)
+    series_id = _sql.Column(_sql.Integer, _sql.ForeignKey("content.catagory_id"))
+    season_no = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    discription = _sql.Column(_sql.String,unique=True,index=True)
+    rating = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    trailer = _sql.Column(_sql.String,unique=True,index=True)
+
+class Season(_database.Base):
+    __tablename__ = "season"
+    season_no = _sql.Column(_sql.Integer, primary_key=True, index=True)
+
+    series = _orm.relationship("Season", back_populates="series.series_id")
+
+class Section(_database.Base):
+    __tablename__ = "Section"
+    name_series = _sql.Column(_sql.String, unique= True, index=True)
+    series = _orm.relationship("Section", back_populates="season.season_no")
+
+class Movie(_database.Base):
+    __tablename__ = "movie"
+    id_movie = _sql.Column(_sql.Integer,unique=True,index=True)
+    name_movie = _sql.Column(_sql.String,unique=True,index=True)
+    movie_length = _sql.Column(_sql.Integer,primary_key=True,index=True)
+    description = _sql.Column(_sql.String,primary_key=True,index=True)
+    actor = _sql.Column(_sql.String,primary_key=True,index=True)
+    trailer = _sql.Column(_sql.String,primary_key=True,index=True)
+    rating = _sql.Column(_sql.Integer,primary_key=True,index=True)
+
+class Payment(_database.Base):
+    __tablename__ = "payment"
+    billing_no = _sql.Column(_sql.Integer,primary_key=True,index=True)
+    amount = _sql.Column(_sql.Float,index=True)
+    payment_method = _sql.Column(_sql.String,index=True)
+    card_number = _sql.Column(_sql.String,index=True)
+    ex_date_card = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+    cvv = _sql.Column(_sql.String,index=True)
+    first_name = _sql.Column(_sql.String,index=True)
+    last_name = _sql.Column(_sql.String,index=True)
+
+
+class Catagory(_database.Base):
+    __tablename__ = "catagory"
+    catagory_id = _sql.Column(_sql.Integer,primary_key=True,index=True)
+    data_base = _sql.Column(_sql.String,unique=True,index=True)
+
+
+class Admin(_database.Base):
+    __tablename__ = "admin"
+    email = _sql.Column(_sql.String,primary_key=True,index=True)
+    password = _sql.Column(_sql.String,unique=True,index=True)
+    phone_number = _sql.Column(_sql.String,unique=True,index=True)
+
+
+class Subscription(_database.Base):
+    __tablename__ = "subscription"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    user_id = _sql.Column(_sql.Integer, _sql.ForeignKey("users.id"))
+    package_type = _sql.Column(_sql.String, index=True)
+    price = _sql.Column(_sql.Integer, index=True)
+    package_detail = _sql.Column(_sql.String)
+    package_ex_date = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+
+    user = _orm.relationship("User", back_populates="subscriptions")
